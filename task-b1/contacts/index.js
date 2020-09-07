5daa63ba54ce2be51e8730b80f659410b835148f
Jeremy Loh <jeremyloh03@gmail.com>
2020-09-07 14:47:32 +0800

    Add body-parser and mongoose

--- task-b1/contacts/index.js
+++ task-b1/contacts/index.js
@@ -7,6 +7,25 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use("/api", apiRoutes);
+// Configure bodyparser to handle post requests
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+mongoose.connect("mongodb://localhost/contacts", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+if (!db) {
+  console.log("Error connecting db");
+} else {
+  console.log("db connected successfully");
+}
+
 // Set default homepage
 app.use("/", (request, response) => {
   response.send("Hello from Express and Nodemon!");
